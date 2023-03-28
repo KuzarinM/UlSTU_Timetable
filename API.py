16ab4653ae924b87649fcd4fba422b837fd01715
KuzarinM <Kuzarinm@mail.ru>
2023-03-28 21:36:12 +0400

    Исправление нескольких косяков для быстрой загрузки на системах типа Ubunta server

--- API.py
+++ API.py
@@ -280,6 +280,7 @@ def Init():
 
 
 if __name__ == '__main__':
+    EEPROM.create_settings_json()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
     #app.run(debug=True)
